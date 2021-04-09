57dbff7d9b3888cb276a07dca96601da6bddc00d
luqmansen <luqmansen@gmail.com>
2021-04-09 09:22:12 +0700

    add anti pod affinity to gosty's k8s manifest

--- apiserver/repositories/mongo/mongo.go
+++ apiserver/repositories/mongo/mongo.go
@@ -2,6 +2,7 @@ package mongo
 
 import (
 	"context"
+	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -17,6 +18,7 @@ type mongoRepository struct {
 func newMongoClient(mongoURL string, mongoTimeout int) (*mongo.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(mongoTimeout)*time.Second)
 	defer cancel()
+	log.Debug("mongourl: ", mongoURL)
 	client, e := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
 	if e != nil {
 		return nil, e
